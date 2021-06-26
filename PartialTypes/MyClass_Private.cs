f5fe89d623e404fc69fcf4025aa78ed2b871f5a3
haprodeptrai123 <68005492+haprodeptrai123@users.noreply.github.com>
2021-06-26 15:02:30 +0700

    1st commit

---
+++ PartialTypes/MyClass_Private.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialTypes
+{
+    public partial class MyClass
+    {
+        // Private data field
+        private double _length;
+        private double _width;
+        private double GetArea()
+        {
+            return _length * _width;
+        }
+
+        // All private helper members.
+        public void Display()
+        {
+            Console.WriteLine("The length is: {0}", _length);
+            Console.WriteLine("The width is: {0}", _width);
+            Console.WriteLine("The area is: {0}", GetArea());
+        }
+    }
+}
---
+++ PartialTypes/MyClass_Public.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialTypes
+{
+    public partial class MyClass
+    {
+        // Constructors.
+        public MyClass(double length, double width)
+        {
+            _length = length;
+            _width = width;
+        }
+
+        // All public members.
+        public double CostCalculation()
+        {
+            double cost;
+            cost = GetArea() * 70;
+            return cost;
+        }
+        public void ShowInformation()
+        {
+            Display();
+            Console.WriteLine("Costs: {0}", CostCalculation());
+        }
+    }
+}
